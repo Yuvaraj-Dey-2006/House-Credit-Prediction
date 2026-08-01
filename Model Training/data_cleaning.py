a4118e1d21964efbe8c1524aadbd22eeb4fa920b
Yuvaraj-Det-2006 <yuvarajdey1234@gmail.com>
2026-08-01 23:18:08 +0530

    Enhance data cleaning script with structured comments and improved readability; remove temporary code file

--- Model Training/data_cleaning.py
+++ Model Training/data_cleaning.py
@@ -1,20 +1,49 @@
-# ============================
-#   IMPORTING LIBRARIES
-# ======================================================================================================================
+#╗ ╝ ╚ ╔ ═ ║ ╬
+
+# ╔══╗   ╔══╗ ╔═══════╗ ██████╗ ███████╗██╗     ███████╗
+# ║  ╚╗ ╔╝  ║╔╝ ╔═══╗ ╚╗██╔══██╗██╔════╝██║     ██╔════╝
+# ║ ╔╗╚═╝╔╗ ║║  ║   ║  ║  ██║█████╗  ██║     ███████╗
+# ║ ║╚╗ ╔╝║ ║║  ║       ║██║  ██║██╔══╝  ██║     ╚════██║
+# ║ ║ ╚═╝ ║ ║╚╗ ╚══  ╔╝██████╔╝███████╗███████╗███████║
+# ╚═╝     ╚═╝ ╚═══════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝
+
 
 import numpy as np
 import pandas as pd
+
 from pathlib import Path
+
+from sklearn.model_selection import train_test_split
+
+                                                        # ███╗   ███╗ ██████╗ ██████╗ ███████╗██╗     ███████╗
+from sklearn.linear_model import ElasticNet             # ████╗ ████║██╔═══██╗██╔══██╗██╔════╝██║     ██╔════╝
+from xgboost import XGBRegressor                        # ██╔████╔██║██║   ██║██║  ██║█████╗  ██║     ███████╗
+from lightgbm import LGBMRegressor                      # ██║╚██╔╝██║██║   ██║██║  ██║██╔══╝  ██║     ╚════██║
+from catboost import CatBoostRegressor                  # ██║ ╚═╝ ██║╚██████╔╝██████╔╝███████╗███████╗███████║
+                                                        # ╚═╝     ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝
 
 from rich.console import Console
 console = Console()
 
-# ============================
-#   DATA LOADING
-# ======================================================================================================================
+# ╔═════════════════════════════════╗
+# ║          DATA LOADING           ║
+# ╚═════════════════════════════════╝════════════════════════════════════════════════════════════════════════════════════════════════════
 
 train_df = pd.read_csv(r"Processed Datasets/final_train.csv")
 
 test_df = pd.read_csv(r"Processed Datasets/final_train.csv")
+
+# ╔════════════════════════════╗
+# ║          MODELS            ║
+# ╚════════════════════════════╝════════════════════════════════════════════════════════════════════════════════════════════════════
+
+training_features = train_df.drop(columns=['TARGET', 'SK_ID_CURR'])
+testing_features = test_df
+target_set = train_df['TARGET']
+
+X_train, X_eval, y_train, y_eval = train_test_split(
+                                        training_features, target_set,
+                                        test_size=0.2, shuffle=True)
+
 
 
--- Model Training/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-train_df = pd.read_csv(r"Processed Datasets/final_train.csv")
-
-test_df = pd.read_csv(r"Processed Datasets/final_train.csv")
